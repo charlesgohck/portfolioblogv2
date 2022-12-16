ffc28c13614dfab94b8a9b2836598d50e7dbebad
Goh, Charles <15852016+charlesgohck@users.noreply.github.com>
2022-12-16 13:29:51 +0800

    [UI] Completed desktop navbar UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,10 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
     return <>
         <div className='navbar' id="topNavBar">
-            <a href="#home" className="navbaroption active navbaroptionselected">Home</a>
+            <a href="#home" className="navbaroption active navbaroptionselected">
+                <img src={"assets/Logo.svg"} alt={"Charles Goh (长康) Logo"} className="faviconlogo"/>
+            </a>
             <a href="#blog" className='navbaroption'>Blog</a>
-            <a href="#about" className='navbaroption'>About</a>
         </div>
         <Component {...pageProps} />
     </>
