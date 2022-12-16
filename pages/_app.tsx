032351ba5e60c8a671eff713a4afaa4d10327dab
Goh, Charles <15852016+charlesgohck@users.noreply.github.com>
2022-12-16 15:20:31 +0800

    [UI] Added font awesome icons for main page and completed first section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import '../styles/globals.css'
-import '../styles/navbar.css'
+import '../styles/kangui.css'
+import '../styles/kanganimations.css'
+import '../styles/imports.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
     return <>
         <div className='navbar' id="topNavBar">
             <img src={"assets/Logo.svg"} alt={"Charles Goh (长康) Logo"} className="faviconlogo" style={{ paddingLeft: "2vh", paddingRight: "2vh"}}/>
-            <a href="#home" className="navbaroption active navbaroptionselected">Home</a>
-            <a href="#blog" className='navbaroption'>Blog</a>
+            <a href="#home" className="navbaroption active navbaroptionselected"><i className='fa solid fa-house-chimney' />&nbsp;&nbsp;Home</a>
+            <a href="#blog" className='navbaroption'><i className="fa-solid fa-book" />&nbsp;&nbsp;Blog</a>
         </div>
-        <Component {...pageProps} />
+        <div className="maincomponent">
+            <Component {...pageProps} />
+        </div>
     </>
 }
