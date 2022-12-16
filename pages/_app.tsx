24c7976b3631e58a00be7d54fabef0b99cd48b27
Goh, Charles <15852016+charlesgohck@users.noreply.github.com>
2022-12-16 13:47:00 +0800

    [UI] Changed color scheme and made navbar stick to top

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,8 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
     return <>
         <div className='navbar' id="topNavBar">
-            <a href="#home" className="navbaroption active navbaroptionselected">
-                <img src={"assets/Logo.svg"} alt={"Charles Goh (长康) Logo"} className="faviconlogo"/>
-            </a>
+            <img src={"assets/Logo.svg"} alt={"Charles Goh (长康) Logo"} className="faviconlogo" style={{ paddingLeft: "2vh", paddingRight: "2vh"}}/>
+            <a href="#home" className="navbaroption active navbaroptionselected">Home</a>
             <a href="#blog" className='navbaroption'>Blog</a>
         </div>
         <Component {...pageProps} />
