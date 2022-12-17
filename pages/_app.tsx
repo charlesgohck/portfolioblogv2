184179a246b212048e83e53190791ee67f2f55ea
Goh, Charles <15852016+charlesgohck@users.noreply.github.com>
2022-12-17 11:25:37 +0800

    [UI][ABOUT] Fixed x index for portfolio main component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
     return <>
         <div className='navbar' id="topNavBar">
-            <img src={"assets/Logo.svg"} alt={"Charles Goh (长康) Logo"} className="faviconlogo" style={{ paddingLeft: "2vh", paddingRight: "2vh"}}/>
+            <img src={"assets/Logo.svg"} alt={"Charles Goh (长康) Logo"} className="faviconlogo" style={{ paddingLeft: "2vh", paddingRight: "2vh" }} />
             <a href="#home" className="navbaroption active navbaroptionselected"><i className='fa solid fa-house-chimney' />&nbsp;&nbsp;About</a>
             <a href="#blog" className='navbaroption'><i className="fa-solid fa-book" />&nbsp;&nbsp;Blog</a>
         </div>
