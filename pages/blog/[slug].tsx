46366c26e91c8548b87d0996afbc9e78672d0080
Goh, Charles <15852016+charlesgohck@users.noreply.github.com>
2022-12-18 12:45:38 +0800

    [UI][BLOG] Completed design of blog post

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -3,7 +3,10 @@ import path from "path";
 import matter from "gray-matter";
 import { ParsedUrlQuery } from 'querystring';
 import { GetStaticProps } from 'next';
-import { convertTypeAcquisitionFromJson } from 'typescript';
+import Head from 'next/head';
+import FadeInSection from '../../components/FadeInSection';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import Link from 'next/link';
 
 interface BlogPageStaticProps {
     params: BlogPageStaticPropsSlugWrapper
@@ -31,16 +34,42 @@ interface IParams extends ParsedUrlQuery {
 }
 
 export default function BlogPage(props: BlogPageProps) {
-
-    console.log(props);
-
     return <>
-        <h1>{props.frontmatter.title}</h1>
-        <h2>{props.frontmatter.date}</h2>
-        <h3>{props.frontmatter.excerpt}</h3>
-        <h4>{props.frontmatter.cover_image}</h4>
-        <br/>
-        <p>{props.content}</p>
+        <Head>
+            <title>Charles Goh (长康): {props.frontmatter.title}</title>
+            <meta name="description" content="Charles Goh (长康) Blog" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/assets/favicon.jpg" />
+        </Head>
+        <main>
+            <section>
+                <article className="blogarticle">
+                    <FadeInSection>
+                        <h1>{props.frontmatter.title}</h1>
+                    </FadeInSection>
+                    <FadeInSection>
+                        <h2>{props.frontmatter.date}</h2>
+                    </FadeInSection>
+                    <FadeInSection>
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <img src={`${props.frontmatter.cover_image}`} alt={`${props.frontmatter.title} post photo`} style={{ width: "80%" }} />
+                        </div>
+                    </FadeInSection>
+                    <FadeInSection>
+                        <div style={{ padding: "5vh" }}>
+                            <ReactMarkdown>{props.content}</ReactMarkdown>
+                        </div>
+                    </FadeInSection>
+                </article>
+            </section>
+            <section className='sectionelement'>
+                <div className='linkelement'>
+                    <Link href={'/blog'}>
+                        <p><strong>Back To Post</strong>&nbsp;&nbsp;<i className="fa solid fa-arrow-right" /></p>
+                    </Link>
+                </div>
+            </section>
+        </main>
     </>
 }
 
