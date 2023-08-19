7a31eb54de62a301c03bddeb29d0620d41807c19
Goh, Charles <15852016+charlesgohck@users.noreply.github.com>
2023-08-19 16:33:46 +0800

    [UI] Removed lazy loading feature

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,16 +1,46 @@
-import dynamic from 'next/dynamic';
 import { InferGetStaticPropsType } from 'next/types';
-import SplashScreen from './SplashScreen';
 import { sortByDate } from '../utils';
 import fs from 'fs';
+import Head from "next/head"
+import { Inter } from "next/font/google"
+import FadeInSection from "../components/FadeInSection";
+import PostLinkElement from "../components/PostLinkElement";
+import BackToPostsSection from "../components/BackToPostsSection";
 
-const LazyBlogComponent = dynamic(() => import("./LazyBlog"), {
-    loading: () => <SplashScreen/>
-})
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Blog({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
     return (
-        <LazyBlogComponent posts={posts} />
+        <>
+            <Head>
+                <title>Charles Goh (伍长康): Blog</title>
+                <meta name="description" content="Charles Goh (伍长康) Blog" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/assets/favicon.jpg" />
+            </Head>
+            <main>
+                <h1 style={{ textAlign: "center", paddingTop: "5vh" }}>Recent</h1>
+                <div className="sectionelement">
+                    {posts.length === 0 
+                    ? <p>No content found...</p>
+                    : posts.map(
+                        post => <div style={{ width: "100%" }} key={`${post.slug} Post Link`}>
+                            <FadeInSection>
+                                <PostLinkElement
+                                    slug={post.slug}
+                                    title={post.frontmatter.title}
+                                    date={post.frontmatter.date}
+                                    excerpt={post.frontmatter.excerpt}
+                                    imagePath={post.frontmatter.cover_image}
+                                    tags={post.frontmatter.tags}
+                                />
+                            </FadeInSection>
+                        </div>
+                    )}
+                </div>
+                <BackToPostsSection showBackToPostsLink={false}/>
+            </main>
+        </>
     )
 }
 
