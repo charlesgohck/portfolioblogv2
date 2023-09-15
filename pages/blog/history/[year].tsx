8001a94907e69276dcff62f3cc0a504216e3a63e
Goh, Charles <15852016+charlesgohck@users.noreply.github.com>
2023-09-15 11:10:35 +0800

    [LOADING] Removed faux loadinga and transferred sorting to component level

--- pages/blog/history/[year].tsx
+++ pages/blog/history/[year].tsx
@@ -1,30 +1,25 @@
 import fs from 'fs';
 import path from "path";
-import matter from "gray-matter";
 import { InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import FadeInSection from '../../../components/FadeInSection';
 import PostLinkElement from '../../../components/PostLinkElement';
 import { Inter } from "next/font/google";
-import { sortByDate } from '../../../utils';
 import BackToPostsSection from '../../../components/BackToPostsSection';
 import { getBlogPostsInfoArray } from '../../blog';
-import { useEffect, useState } from 'react';
-import FauxLoadingElement from '../../FauxLoadingElement';
+import { sortByDate } from '../../../utils';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function BlogFilterByTags({ year, posts }: InferGetStaticPropsType<typeof getStaticProps>) {
 
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 2000);
-    }, [])
-
     const title: string = `Charles Goh (伍长康): Blog History for ${year}`;
+
+    posts = posts.filter(post => {
+        const postDate: Date = new Date(post.frontmatter.date);
+        const postDateYear: number = postDate.getUTCFullYear();
+        return postDateYear.toString() === year;
+    }).sort(sortByDate);
 
     return (
         <>
@@ -36,27 +31,23 @@ export default function BlogFilterByTags({ year, posts }: InferGetStaticPropsTyp
             </Head>
             <main>
                 <h1 style={{ textAlign: "center", paddingTop: "5vh" }}>Posts in: {year}</h1>
-                {
-                    isLoading ?
-                    <FauxLoadingElement />
-                    : <div className="sectionelement">
-                        {posts.length == 0
-                        ? <p>No content found...</p>
-                        : posts.map(post => <div style={{ width: "100%" }} key={`${post.slug} Post Link`}>
-                                <FadeInSection>
-                                    <PostLinkElement
-                                        slug={post.slug}
-                                        title={post.frontmatter.title}
-                                        date={post.frontmatter.date}
-                                        excerpt={post.frontmatter.excerpt}
-                                        imagePath={post.frontmatter.cover_image}
-                                        tags={post.frontmatter.tags}
-                                    />
-                                </FadeInSection>
-                            </div>
-                        )}
-                    </div>
-                }
+                <div className="sectionelement">
+                    {posts.length == 0
+                    ? <p>No content found...</p>
+                    : posts.map(post => <div style={{ width: "100%" }} key={`${post.slug} Post Link`}>
+                            <FadeInSection>
+                                <PostLinkElement
+                                    slug={post.slug}
+                                    title={post.frontmatter.title}
+                                    date={post.frontmatter.date}
+                                    excerpt={post.frontmatter.excerpt}
+                                    imagePath={post.frontmatter.cover_image}
+                                    tags={post.frontmatter.tags}
+                                />
+                            </FadeInSection>
+                        </div>
+                    )}
+                </div>
                 <BackToPostsSection />
             </main>
         </>
@@ -93,16 +84,10 @@ export async function getStaticProps(context: any) {
     // More efficient implementation for getting posts
     var posts = getBlogPostsInfoArray(true);
 
-    posts = posts.filter(post => {
-        const postDate: Date = new Date(post.frontmatter.date);
-        const postDateYear: number = postDate.getUTCFullYear();
-        return postDateYear.toString() === year;
-    });
-
     return {
         props: {
             year: year,
-            posts: posts.sort(sortByDate),
+            posts: posts
         },
     }
 }
